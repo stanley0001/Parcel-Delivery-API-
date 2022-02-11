4e519ce3ff557b05d907506a7e22450c0883de3f
stanley0001 <mungaistanley001@gmail.com>
2022-02-11 14:35:13 +0300

    dev

--- src/main/java/com/stan/parcel/Controllers/Communication/CommunicationController.java
+++ src/main/java/com/stan/parcel/Controllers/Communication/CommunicationController.java
@@ -4,6 +4,7 @@ import com.stan.parcel.Percistance.Entities.Message;
 import com.stan.parcel.Percistance.Model.Notification;
 import com.stan.parcel.Percistance.Model.ResponseModel;
 import com.stan.parcel.ServiceImplementation.MessageService;
+import org.hibernate.mapping.Any;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -31,6 +32,12 @@ public class CommunicationController {
         ResponseModel response=communicationService.createMessage(notification);
 
         return new ResponseEntity<>(response,response.getStatus());
+    }
+    @PostMapping("receiveCallBack")
+    public ResponseEntity<Any> sendNotification( Any notification){
+
+
+        return new ResponseEntity<>(notification, HttpStatus.OK);
     }
 }
 
